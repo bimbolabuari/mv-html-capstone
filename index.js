255e683a1394260ed6724a3e72fde320b9da4c20
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-07-01 14:04:23 +0100

    feat: Add featured speaker dynamically

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const close = document.querySelector('#close');
 const hamburger = document.querySelector('#open');
 const nav = document.querySelector('#nav');
+const featuredContainer = document.querySelector('.featured-ul');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.add('none');
@@ -14,4 +15,66 @@ close.addEventListener('click', () => {
   nav.classList.remove('nav-list');
   hamburger.classList.remove('none');
   close.classList.add('none');
+});
+
+const speakersArray = [
+  {
+    name: 'Yochai Benkler',
+    intro: 'Berkamn Professor of Enterpreneurial Legal studies at Harvard Law school',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: './images/passport.png',
+  },
+  {
+    name: 'Yochai Benkler',
+    introduction: 'Berkamn Professor of Enterpreneurial Legal studies at Harvard Law school',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: './images/passport.png',
+  },
+  {
+    name: 'Yochai Benkler',
+    introduction: 'Berkamn Professor of Enterpreneurial Legal studies at Harvard Law school',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: './images/passport.png',
+  },
+  {
+    name: 'Yochai Benkler',
+    introduction: 'Berkamn Professor of Enterpreneurial Legal studies at Harvard Law school',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: './images/passport.png',
+  },
+  {
+    name: 'Yochai Benkler',
+    introduction: 'Berkamn Professor of Enterpreneurial Legal studies at Harvard Law school',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: './images/passport.png',
+  },
+  {
+    name: 'Yochai Benkler',
+    introduction: 'Berkamn Professor of Enterpreneurial Legal studies at Harvard Law school',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: './images/passport.png',
+  },
+];
+
+const createFeaturedSpeaker = (speaker) => {
+  const speakerContainer = document.createElement('li');
+  speakerContainer.classList.add('featured-list', 'flex');
+  speakerContainer.innerHTML = `
+  <img src='${speaker.image}' class="featured-img" alt="featured-speakers">
+  <div class="featured-container">
+      <h3 class="featured-h3">${speaker.name}</h3>
+      <h4 class="featured-h4">${speaker.intro}</h4>
+      <p class="featured-paragraph">${speaker.about}</p>
+  </div>
+  `;
+  return speakerContainer;
+};
+
+const showSpeaker = (speaker, featuredContainer) => {
+  const newFeaturedSpeaker = createFeaturedSpeaker(speaker);
+  featuredContainer.appendChild(newFeaturedSpeaker);
+};
+
+speakersArray.forEach((speaker) => {
+  showSpeaker(speaker, featuredContainer);
 });
